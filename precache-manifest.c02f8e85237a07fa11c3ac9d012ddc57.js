2640f3f7e66e021fd7251e96de9db9e61af3f3cd
Zuzanna Kiljańska <zuza.ostrowska@gmail.com>
2020-07-03 21:45:15 +0200

    Updates

---
+++ precache-manifest.c02f8e85237a07fa11c3ac9d012ddc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c7cc62ce28a4e0764181de62a830ea",
+    "url": "/page-nasa-api/index.html"
+  },
+  {
+    "revision": "946108b1584f66ce7e2f",
+    "url": "/page-nasa-api/static/css/main.f943b3f7.chunk.css"
+  },
+  {
+    "revision": "71fbb048d870b9980990",
+    "url": "/page-nasa-api/static/js/2.a6f0d208.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/page-nasa-api/static/js/2.a6f0d208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946108b1584f66ce7e2f",
+    "url": "/page-nasa-api/static/js/main.d740621f.chunk.js"
+  },
+  {
+    "revision": "c2d391f01018e67d1cc2",
+    "url": "/page-nasa-api/static/js/runtime-main.2225c285.js"
+  }
+]);
